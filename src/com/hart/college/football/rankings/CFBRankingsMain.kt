f2f1940a75c1113e0dec94cb18df8072681fc241
Alden Hart <alden.hart9@gmail.com>
2016-10-12 19:16:19 -0400

    Initial commit for college football rankings

---
+++ src/com/hart/college/football/rankings/CFBRankingsMain.kt
@@ -0,0 +1,9 @@
+package com.hart.college.football.rankings
+
+/** Main class to play around with a college football rankings system.
+ * Created by alhart on 10/12/16.
+ */
+
+fun main(args: Array<String>) {
+    println("Here we go")
+}
